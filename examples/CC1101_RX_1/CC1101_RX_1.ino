543ce6b73001f8a8400af5ce3c523dc21226530d
battosai30 <batto@hotmail.fr>
2014-05-03 19:34:59 +0200

    Update CC1101_RX_1.ino

--- examples/CC1101_RX_1/CC1101_RX_1.ino
+++ examples/CC1101_RX_1/CC1101_RX_1.ino
@@ -5,36 +5,37 @@ void setup()
 {
 
   Serial.begin(9600);
-
-  pinMode(P1_3,INPUT); // GP0 pin
+  Serial.println("Launching");
+  
+  pinMode(5,INPUT); // GD0 pin
 
   OPEN_rf.Init(10); // chip select on pin 10
 
   OPEN_rf.SetModulation(MODULATION_ASK_OOK);
-  OPEN_rf.RXModeOff(RXOFF_MODE_STAYINRX);
+  OPEN_rf.RXModeOff(RXOFF_MODE_STAYINRX); 
   OPEN_rf.EnableAdressCheck(TRUE);
   OPEN_rf.SetAdress(23);
   OPEN_rf.EnableCRC(FALSE);
 
-  Serial.println("Launching");
-
   OPEN_rf.SetRX();
+
+  Serial.println("RX Enabled");
 
 }
 
 void loop() {
 
-  if(digitalRead(P1_3)) {
+  if(digitalRead(5)) {
 
     if(OPEN_rf.BytesAvailableInRXFIFO()){
 
       delay(20);
-      
+
       byte Buffer[20];
-      
+
       Serial.print("Bytes in RX = ");
       Serial.println(OPEN_rf.BytesAvailableInRXFIFO());
-      
+
       int y = OPEN_rf.ReceiveData(Buffer);
 
       Serial.print("Received : ");
@@ -46,6 +47,15 @@ void loop() {
   }
 
 }
+
+
+
+
+
+
+
+
+
 
 
 
